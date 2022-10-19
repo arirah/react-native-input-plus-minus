8589f6ca16f6778eb0020e19bf2c2e53638d50ff
Arifur <md.arifur.rahman@intellier.com>
2022-10-19 17:24:49 +0600

    Initial commit

---
+++ example/App.js
@@ -0,0 +1,106 @@
+import React, {Component} from 'react';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Counters from 'react-native-counters';
+
+export default class Index extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <View style={Styles.content}>
+          <Text style={Styles.title}>Default Styles</Text>
+          <Counters />
+        </View>
+        <View style={Styles.content}>
+          <Text style={Styles.title}>Custom Styles</Text>
+          <Counters
+            buttonStyle={{
+              borderColor: '#333',
+              borderWidth: 2,
+            }}
+            buttonTextStyle={{
+              color: '#333',
+            }}
+            countTextStyle={{
+              color: '#333',
+            }}
+            onChange={(len, type) => {
+              console.log(len, type);
+            }}
+          />
+
+          <View style={{marginTop: 15}}>
+            <Counters
+              buttonStyle={{
+                borderColor: '#333',
+                borderWidth: 2,
+                borderRadius: 25,
+              }}
+              buttonTextStyle={{
+                color: '#333',
+              }}
+              countTextStyle={{
+                color: '#333',
+              }}
+            />
+          </View>
+        </View>
+        <View style={Styles.content}>
+          <Text style={Styles.title}>Custom Icons</Text>
+          <Counters
+            minusIcon={() => <Icon name="minus" size={20} color="#27AAE1" />}
+            plusIcon={() => <Icon name="plus" size={20} color="#27AAE1" />}
+          />
+        </View>
+
+        <View style={Styles.content}>
+          <Text style={Styles.title}>Custom Texts</Text>
+          <Counters
+            plus={'Add'}
+            minus={'Remove'}
+            buttonStyle={{
+              width: 100
+            }}
+          />
+        </View>
+
+
+        <View style={Styles.content}>
+          <Text style={Styles.title}>On Change Before</Text>
+          <Counters
+            plus={'Add'}
+            minus={'Remove'}
+            buttonStyle={{
+              width: 100
+            }}
+
+            onChange={(len, type) => {
+              console.log(len, type);
+            }}
+
+            onChangeBefore={(success) => {
+
+              // Fetch function 
+              setTimeout(() => {
+                success()
+              }, 5000)
+            }}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const Styles = StyleSheet.create({
+  content: {
+    paddingVertical: 10,
+    alignItems: 'center',
+  },
+  title: {
+    paddingVertical: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
